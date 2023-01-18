c7b13251944857703ea96bf04191032d9205faa6
stefan-yas <stefan.sy.yas@gmail.com>
2023-01-18 19:28:10 +0100

    refactor: contact links section is now a proper svelte component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,76 @@
 <script>
+	import Contacts from './contacts.svelte';
 	const title = "Hi, I'm Stefan";
-	const telegramIcon = '/telegram_icon.svg';
-	const elementIcon = '/element-messenger.svg';
-	const emailIcon = '/email_icon.svg';
-	const githubLogo = '/github logo.svg';
-	const threemaIcon = '/threema-messenger.svg';
-	const sessionIcon = '/session_icon.svg';
+
+	const telegram = {
+		href: 'https://stefanyas.t.me',
+		src: '/telegram_icon.svg',
+		alt: 'telegram-icon',
+		name: 'Telegram'
+	};
+
+	const email = {
+		href: 'mailto:stefan@stefanyas.me',
+		src: '/email_icon.svg',
+		alt: 'email-icon',
+		name: 'Email'
+	};
+
+	const matrix = {
+		href: 'https://matrix.to/#/@stefan_yas:matrix.org',
+		src: '/element-messenger.svg',
+		alt: 'element-icon',
+		name: 'Matrix'
+	};
+
+	const threema = {
+		href: 'https://threema.id/5AU7KBY5',
+		src: '/threema-messenger.svg',
+		alt: 'threema-icon',
+		name: 'Threema'
+	};
+
+	const session = {
+		href: '#session-id-copied-to-clipboard',
+		src: '/session_icon.svg',
+		alt: 'session-icon',
+		name: 'Session',
+		id: 'session-id'
+	};
+
+	const github = {
+		href: 'https://github.com/stefan-yas',
+		src: '/github logo.svg',
+		alt: 'github-icon',
+		name: 'GitHub'
+	};
 </script>
 
 <head>
 	<title>{title}</title>
 </head>
+<main>
+	<h1>{title}</h1>
 
-<body>
-	<main>
-		<h1>{title}</h1>
-
-		<section class="block">
-			<div class="contacts">
-				<a href="https://stefanyas.t.me" target="_blank" rel="noreferrer"
-					><img src={telegramIcon} alt="telegram-icon" />
-					<p>Telegram</p></a
-				>
-				<a href="mailto:stefan@stefanyas.me" target="_blank" rel="noreferrer"
-					><img src={emailIcon} alt="email-icon" />
-					<p>Email</p></a
-				>
-				<a href="https://matrix.to/#/@stefan_yas:matrix.org" target="_blank" rel="noreferrer"
-					><img src={elementIcon} alt="element-icon" />
-					<p>Matrix</p></a
-				>
-				<a href="https://threema.id/5AU7KBY5" target="_blank" rel="noreferrer"
-					><img src={threemaIcon} alt="threema-icon" />
-					<p>Threema</p></a
-				>
-				<a id="session-id" href="/#session-id-copied-to-clipboard"
-					><img src={sessionIcon} alt="session-icon" />
-					<p>Session</p></a
-				>
-				<a href="https://github.com/stefan-yas" target="_blank" rel="noreferrer"
-					><img src={githubLogo} alt="github-logo" />
-					<p>GitHub</p></a
-				>
-			</div>
-		</section>
-	</main>
-</body>
+	<section class="block">
+		<div class="contacts">
+			<Contacts href={telegram.href} src={telegram.src} alt={telegram.alt} name={telegram.name} />
+			<Contacts href={email.href} src={email.src} alt={email.alt} name={email.name} />
+			<Contacts href={matrix.href} src={matrix.src} alt={matrix.alt} name={matrix.name} />
+			<Contacts href={threema.href} src={threema.src} alt={threema.alt} name={threema.name} />
+			<Contacts
+				id={session.id}
+				href={session.href}
+				src={session.src}
+				alt={session.alt}
+				name={session.name}
+			/>
+			<Contacts href={github.href} src={github.src} alt={github.alt} name={github.name} />
+		</div>
+	</section>
+</main>
 
 <style>
-
 	h1 {
 		font-size: 5em;
 	}
@@ -72,57 +94,14 @@
 		gap: 2em;
 	}
 
-	.contacts a {
-		text-decoration: none;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		gap: 1em;
-	}
-
-	.contacts a:visited {
-		text-decoration: none;
-		color: var(--secondary-font-color);
-	}
-
-	.contacts a p {
-		margin: 0px;
-		color: var(--secondary-font-color);
-		font-weight: bold;
-	}
-
-	.contacts a img {
-		width: 5em;
-	}
-
-	.contacts a:hover {
-		transform: scale(1.01);
-	}
-
-	#session-id:hover:after {
-		content: url(/session.png);
-		width: 125px;
-		display: block;
-		position: absolute;
-		top: 8em;
-	}
-
 	@media only screen and (max-width: 900px) {
 		h1 {
 			font-size: 2em;
 		}
+
 		.contacts {
 			flex-direction: column;
 			align-items: flex-start;
-		}
-
-		.contacts a {
-			flex-direction: row;
-		}
-
-		.contacts a img {
-			width: 3em;
 		}
 	}
 </style>
---
+++ src/routes/contacts.svelte
@@ -0,0 +1,60 @@
+<script>
+	export let href;
+	export let src;
+	export let alt;
+	export let name;
+	export let id;
+</script>
+
+<a {id} {href} target="_blank" rel="noreferrer"
+	><img {src} {alt} />
+	<p>{name}</p></a
+>
+
+<style>
+	a {
+		text-decoration: none;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		gap: 1em;
+	}
+
+	a:visited {
+		text-decoration: none;
+		color: var(--secondary-font-color);
+	}
+
+	a:hover {
+		transform: scale(1.01);
+	}
+
+	a p {
+		margin: 0px;
+		color: var(--secondary-font-color);
+		font-weight: bold;
+	}
+
+	a img {
+		width: 5em;
+	}
+
+	#session-id:hover:after {
+		content: url(/session.png);
+		width: 125px;
+		display: block;
+		position: absolute;
+		top: 8em;
+	}
+
+	@media only screen and (max-width: 900px) {
+		a {
+			flex-direction: row;
+		}
+
+		a img {
+			width: 3em;
+		}
+	}
+</style>
